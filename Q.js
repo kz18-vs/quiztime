a9412e22721e2355e5ee1d0e3ffb0c4feebc7777
unknown <kzarmoune@gmail.com>
2025-09-04 12:04:12 +0100

    first commit

---
+++ Q.js
@@ -0,0 +1,363 @@
+questions = {
+  "countries": [
+    {
+      "question": "Quel est le plus grand pays du monde par superficie ?",
+      "answers": ["Chine", "États-Unis", "Russie", "Canada"],
+      "correct_answer": "Russie"
+    },
+    {
+      "question": "Quelle est la capitale de l’Australie ?",
+      "answers": ["Sydney", "Canberra", "Melbourne", "Perth"],
+      "correct_answer": "Canberra"
+    },
+    {
+      "question": "Quel est le pays le plus peuplé au monde?",
+      "answers": ["Inde", "Chine", "États-Unis", "Indonésie"],
+      "correct_answer": "Chine"
+    },
+    {
+      "question": "Quelle est la capitale du Maroc ?",
+      "answers": ["Marrakech", "Casablanca", "Rabat", "Fès"],
+      "correct_answer": "Rabat"
+    },
+    {
+      "question": "Quel pays est le plus peuplé au monde en 2025 ?",
+      "answers": ["Chine", "Inde", "États-Unis", "Indonésie"],
+      "correct_answer": "Inde"
+    },
+    {
+      "question": "Quel est le seul pays à être à la fois un continent ?",
+      "answers": ["Canada", "Australie", "Russie", "Brésil"],
+      "correct_answer": "Australie"
+    },
+    {
+      "question": "Quelle langue est parlée au Brésil ?",
+      "answers": ["Espagnol", "Portugais", "Français", "Anglais"],
+      "correct_answer": "Portugais"
+    },
+    {
+      "question": "Lequel de ces pays est enclavé (sans accès à la mer) ?",
+      "answers": ["Bolivie", "Équateur", "Thaïlande", "Norvège"],
+      "correct_answer": "Bolivie"
+    },
+    {
+      "question": "Dans quel pays se trouve la ville du Caire ?",
+      "answers": ["Tunisie", "Égypte", "Maroc", "Arabie Saoudite"],
+      "correct_answer": "Égypte"
+    },
+    {
+      "question": "Quelle est la monnaie officielle du Japon ?",
+      "answers": ["Yuan", "Yen", "Won", "Ringgit"],
+      "correct_answer": "Yen"
+    }
+  ],
+  
+  "history": [
+    {
+      "question": "En quelle année a eu lieu la Révolution française ?",
+      "answers": ["1776", "1789", "1804", "1848"],
+      "correct_answer": "1789"
+    },
+    {
+      "question": "Qui était le premier empereur romain ?",
+      "answers": ["Jules César", "Auguste", "Néron", "Trajan"],
+      "correct_answer": "Auguste"
+    },
+    {
+      "question": "Quel roi français est surnommé le « Roi Soleil » ?",
+      "answers": ["François Ier", "Henri IV", "Louis XIV", "Louis XVI"],
+      "correct_answer": "Louis XIV"
+    },
+    {
+      "question": "Quel événement marque le début de la Seconde Guerre mondiale ?",
+      "answers": [
+        "L’attaque de Pearl Harbor",
+        "Le débarquement de Normandie",
+        "L’invasion de la Pologne",
+        "La bataille de Stalingrad"
+      ],
+      "correct_answer": "L’invasion de la Pologne"
+    },
+    {
+      "question": "Qui a découvert l’Amérique en 1492 ?",
+      "answers": [
+        "Vasco de Gama",
+        "Ferdinand Magellan",
+        "Christophe Colomb",
+        "Marco Polo"
+      ],
+      "correct_answer": "Christophe Colomb"
+    },
+    {
+      "question": "Quelle civilisation a construit les pyramides de Gizeh ?",
+      "answers": ["Les Romains", "Les Grecs", "Les Égyptiens", "Les Mayas"],
+      "correct_answer": "Les Égyptiens"
+    },
+    {
+      "question": "Qui a dirigé l’URSS pendant la Seconde Guerre mondiale ?",
+      "answers": ["Lénine", "Gorbatchev", "Staline", "Trotski"],
+      "correct_answer": "Staline"
+    },
+    {
+      "question": "Quel empire a dominé l’Europe au XIXe siècle sous Napoléon ?",
+      "answers": [
+        "L’Empire britannique",
+        "L’Empire austro-hongrois",
+        "L’Empire russe",
+        "L’Empire français"
+      ],
+      "correct_answer": "L’Empire français"
+    },
+    {
+      "question": "En quelle année a été signée la Déclaration des droits de l’homme et du citoyen en France ?",
+      "answers": ["1789", "1792", "1801", "1848"],
+      "correct_answer": "1789"
+    },
+    {
+      "question": "Quel mur est tombé en 1989, marquant la fin de la guerre froide ?",
+      "answers": [
+        "Mur d’Hadrien",
+        "Mur de Chine",
+        "Mur de Berlin",
+        "Mur de Varsovie"
+      ],
+      "correct_answer": "Mur de Berlin"
+    }
+  ],
+
+  "computersc": [
+    {
+      "question": "Que signifie « CPU » ?",
+      "answers": [
+        "Central Process Unit",
+        "Central Processing Unit",
+        "Computer Program Unit",
+        "Control Program Utility"
+      ],
+      "correct_answer": "Central Processing Unit"
+    },
+    {
+      "question": "Quel langage est principalement utilisé pour le développement web côté client ?",
+      "answers": ["Java", "Python", "JavaScript", "C++"],
+      "correct_answer": "JavaScript"
+    },
+    {
+      "question": "Quelle est la base du système binaire utilisé en informatique ?",
+      "answers": ["8", "2", "10", "16"],
+      "correct_answer": "2"
+    },
+    {
+      "question": "Quel composant stocke temporairement les données pendant le traitement ?",
+      "answers": ["Disque dur", "RAM", "ROM", "Clavier"],
+      "correct_answer": "RAM"
+    },
+    {
+      "question": "Lequel de ces systèmes d’exploitation est open source ?",
+      "answers": ["Windows", "macOS", "Linux", "iOS"],
+      "correct_answer": "Linux"
+    },
+    {
+      "question": "Que signifie \"HTML\" ?",
+      "answers": [
+        "HighText Machine Language",
+        "HyperText Markup Language",
+        "Hyperloop Machine Language",
+        "Hyper Tool Markup Line"
+      ],
+      "correct_answer": "HyperText Markup Language"
+    },
+    {
+      "question": "Quel protocole est utilisé pour naviguer sur le web ?",
+      "answers": ["FTP", "HTTP", "SMTP", "IP"],
+      "correct_answer": "HTTP"
+    },
+    {
+      "question": "Une boucle « for » est utilisée pour :",
+      "answers": [
+        "Créer un fichier",
+        "Lire un fichier",
+        "Répéter une série d’instructions",
+        "Déboguer un code"
+      ],
+      "correct_answer": "Répéter une série d’instructions"
+    },
+    {
+      "question": "En informatique, que signifie le terme \"bug\" ?",
+      "answers": [
+        "Une mise à jour",
+        "Une erreur dans le code",
+        "Un virus informatique",
+        "Un fichier caché"
+      ],
+      "correct_answer": "Une erreur dans le code"
+    },
+    {
+      "question": "Quel est le langage de requête utilisé pour interagir avec des bases de données relationnelles ?",
+      "answers": ["XML", "SQL", "CSS", "JSON"],
+      "correct_answer": "SQL"
+    }
+  ],
+  
+  "math": [
+    {
+      "question": "Quel est le plus grand nombre parmi les suivants ?",
+      "answers": ["87", "105", "99", "100"],
+      "correct_answer": "105"
+    },
+    {
+      "question": "Combien y a-t-il de dizaines dans 340 ?",
+      "answers": ["34", "3", "340", "30"],
+      "correct_answer": "34"
+    },
+    {
+      "question": "Quel est le chiffre des unités dans le nombre 1 276 ?",
+      "answers": ["6", "7", "1", "2"],
+      "correct_answer": "6"
+    },
+    {
+      "question": "Quel nombre est un multiple de 5 ?",
+      "answers": ["12", "18", "25", "33"],
+      "correct_answer": "25"
+    },
+    {
+      "question": "Quel est le successeur de 199 ?",
+      "answers": ["198", "200", "190", "210"],
+      "correct_answer": "200"
+    },
+    {
+      "question": "Quel est le double de 48 ?",
+      "answers": ["86", "92", "96", "88"],
+      "correct_answer": "96"
+    },
+    {
+      "question": "Quel est le nombre impair ?",
+      "answers": ["18", "30", "27", "44"],
+      "correct_answer": "27"
+    },
+    {
+      "question": "Combien font 7 × 8 ?",
+      "answers": ["54", "56", "58", "64"],
+      "correct_answer": "56"
+    },
+    {
+      "question": "Quel est le plus petit nombre premier ?",
+      "answers": ["0", "1", "2", "3"],
+      "correct_answer": "2"
+    },
+    {
+      "question": "Quel nombre correspond à “trois centaines, deux dizaines et quatre unités” ?",
+      "answers": ["324", "342", "234", "423"],
+      "correct_answer": "324"
+    }
+  ],
+  
+  "geologie": [
+    {
+      "question": "Quel est le principal composant de la croûte terrestre ?",
+      "answers": ["Basalte", "Quartz", "Silicium", "Granit"],
+      "correct_answer": "Silicium"
+    },
+    {
+      "question": "Comment s'appelle la couche semi-solide située sous la lithosphère ?",
+      "answers": ["Asthénosphère", "Manteau supérieur", "Noyau externe", "Croûte océanique"],
+      "correct_answer": "Asthénosphère"
+    },
+    {
+      "question": "Quel type de roche est formé par le refroidissement du magma ?",
+      "answers": ["Roche sédimentaire", "Roche métamorphique", "Roche ignée", "Roche fossile"],
+      "correct_answer": "Roche ignée"
+    },
+    {
+      "question": "Quel est le nom du supercontinent qui existait il y a 250 millions d’années ?",
+      "answers": ["Laurasia", "Pangée", "Gondwana", "Eurasia"],
+      "correct_answer": "Pangée"
+    },
+    {
+      "question": "Quelle échelle est utilisée pour mesurer la magnitude des séismes ?",
+      "answers": ["Échelle de Beaufort", "Échelle de Mercalli", "Échelle de Richter", "Échelle de Mohs"],
+      "correct_answer": "Échelle de Richter"
+    },
+    {
+      "question": "Quelle roche est principalement composée de calcite ?",
+      "answers": ["Granit", "Basalte", "Marbre", "Gneiss"],
+      "correct_answer": "Marbre"
+    },
+    {
+      "question": "Comment s'appelle l’étude des couches géologiques ?",
+      "answers": ["Paléontologie", "Pétrologie", "Stratigraphie", "Tectonique"],
+      "correct_answer": "Stratigraphie"
+    },
+    {
+      "question": "Quelle est la couche la plus interne de la Terre ?",
+      "answers": ["Croûte", "Manteau", "Noyau externe", "Noyau interne"],
+      "correct_answer": "Noyau interne"
+    },
+    {
+      "question": "Comment appelle-t-on le processus de transformation d'une roche par la chaleur et la pression ?",
+      "answers": ["Érosion", "Métamorphisme", "Fusion", "Solidification"],
+      "correct_answer": "Métamorphisme"
+    },
+    {
+      "question": "Quel est le minéral le plus dur selon l’échelle de Mohs ?",
+      "answers": ["Quartz", "Topaze", "Diamant", "Feldspath"],
+      "correct_answer": "Diamant"
+    }
+  ],
+
+  "hydrologie": [
+    {
+      "question": "Quel est le principal réservoir d'eau douce sur Terre ?",
+      "answers": ["Les rivières", "Les glaciers", "Les lacs", "L'atmosphère"],
+      "correct_answer": "Les glaciers"
+    },
+    {
+      "question": "Comment s'appelle le cycle de l'eau dans la nature ?",
+      "answers": ["Cycle hydrique", "Cycle de vie", "Cycle océanique", "Cycle thermique"],
+      "correct_answer": "Cycle hydrique"
+    },
+    {
+      "question": "Quelle est la principale cause des marées ?",
+      "answers": ["Le Soleil", "Le vent", "La gravité de la Lune", "Les courants marins"],
+      "correct_answer": "La gravité de la Lune"
+    },
+    {
+      "question": "Comment s'appelle la nappe d'eau souterraine contenue dans des roches perméables ?",
+      "answers": ["Geyser", "Réseau karstique", "Aquifère", "Torrent"],
+      "correct_answer": "Aquifère"
+    },
+    {
+      "question": "Quel fleuve est le plus long du monde ?",
+      "answers": ["Nil", "Amazonie", "Yangtsé", "Mississippi"],
+      "correct_answer": "Amazonie"
+    },
+    {
+      "question": "Quelle est la forme principale de précipitation dans les régions tempérées ?",
+      "answers": ["Pluie", "Neige", "Grêle", "Brouillard"],
+      "correct_answer": "Pluie"
+    },
+    {
+      "question": "Quel est le terme pour désigner l’infiltration de l’eau dans le sol ?",
+      "answers": ["Ruissellement", "Absorption", "Percolation", "Évaporation"],
+      "correct_answer": "Percolation"
+    },
+    {
+      "question": "Quelle mer est connue pour être l’une des plus salées au monde ?",
+      "answers": ["Mer Baltique", "Mer Noire", "Mer Morte", "Mer Rouge"],
+      "correct_answer": "Mer Morte"
+    },
+    {
+      "question": "Comment appelle-t-on l’eau contenue dans l’atmosphère ?",
+      "answers": ["Eau fossile", "Eau météorique", "Eau atmosphérique", "Vapeur d’eau"],
+      "correct_answer": "Vapeur d’eau"
+    },
+    {
+      "question": "Quel est l’état physique de l’eau dans les nuages ?",
+      "answers": ["Liquide", "Solide", "Gazeux", "Plasma"],
+      "correct_answer": "Liquide"
+    }
+  ]
+}
+
+
+
+
